e25336fc9e9fa49046b56301eccbb59529bea9ad
Quentin Herzig <quentin.herzig@gmail.com>
2016-03-22 18:04:15 +0100

    gps at 30Hz + gps status

--- src/navio2_gps/src/gps_pub.cpp
+++ src/navio2_gps/src/gps_pub.cpp
@@ -36,17 +36,79 @@ void init_gps_msg(sensor_msgs::NavSatFix* gps_msg)
 	{
 		gps_msg->position_covariance[i] = !(i%4);
 	}
+
+	gps_msg->position_covariance_type = 0;
 }
 
-void update_gps_msg(sensor_msgs::NavSatFix* gps_msg, std::vector<double> pos_data)
+void update_gps_msg(sensor_msgs::NavSatFix* gps_msg, std::vector<double> pos_data, Ublox gps)
 {
 	gps_msg->header.stamp = ros::Time::now();
 
-	gps_msg->latitude = pos_data[2]/10000000;
-	gps_msg->longitude = pos_data[1]/10000000;
-	gps_msg->altitude = pos_data[3]/1000;
+	if (gps.decodeSingleMessage(Ublox::NAV_POSLLH, pos_data) == 1)
+    {
+		gps_msg->latitude = pos_data[2]/10000000;
+		gps_msg->longitude = pos_data[1]/10000000;
+		gps_msg->altitude = pos_data[3]/1000;
 
-	ROS_INFO("GPS : Lat = %f, Long = %f, Alt = %f", pos_data[2]/10000000, pos_data[1]/10000000, pos_data[3]/1000);
+		ROS_INFO("GPS : Lat = %f, Long = %f, Alt = %f", pos_data[2]/10000000, pos_data[1]/10000000, pos_data[3]/1000);
+	}
+
+	else 
+	{
+      ROS_INFO("Message not captured");
+    }
+
+
+    if (gps.decodeSingleMessage(Ublox::NAV_STATUS, pos_data) == 1)
+    {
+        ROS_INFO("Current GPS status:");
+        ROS_INFO("gpsFixOk: %d", ((int)pos_data[1] & 0x01));
+
+        ROS_INFO("gps Fix status: ");
+        switch((int)pos_data[0]){
+            case 0x00:
+            	gps_msg->status = -1;
+                ROS_INFO("no fix");
+                break;
+
+            case 0x01:
+            	gps_msg->status = -1;
+                ROS_INFO("dead reckoning only");
+                break;
+
+            case 0x02:
+            	gps_msg->status = 0;
+                ROS_INFO("2D-fix");
+                break;
+
+            case 0x03:
+            	gps_msg->status = 0;
+                ROS_INFO("3D-fix");
+                break;
+
+            case 0x04:
+            	gps_msg->status = -1;
+                ROS_INFO("GPS + dead reckoning combined");
+                break;
+
+            case 0x05:
+            	gps_msg->status = -1;
+                ROS_INFO("Time only fix");
+                break;
+
+            default:
+            	gps_msg->status = -1;
+                ROS_INFO("Reserved value. Current state unknown");
+                break;
+
+        }
+
+
+   else 
+   {
+        ROS_INFO("Status Message not captured");
+   }
+
 }
 
 int main(int argc, char *argv[]){
@@ -55,7 +117,7 @@ int main(int argc, char *argv[]){
 	ros::NodeHandle n;
 	ros::Publisher gps_pub = n.advertise<sensor_msgs::NavSatFix>("gps_readings", 1000);
 
-	ros::Rate loop_rate(2);
+	ros::Rate loop_rate(30);
 
     if (check_apm()) {
         return 1;
@@ -88,81 +150,29 @@ int main(int argc, char *argv[]){
         // In this example we decode NAV_STATUS and NAV-POSLLH messages, adding new types, however is quite easy
 
         while (ros::ok())
-        {		
+        {	
+            sensor_msgs::NavSatFix gps_msg;
+            init_gps_msg(&gps_msg);	
 
-            if (gps.decodeSingleMessage(Ublox::NAV_POSLLH, pos_data) == 1)
-            {
-            		sensor_msgs::NavSatFix gps_msg;
-            		init_gps_msg(&gps_msg);
 
-            		update_gps_msg(&gps_msg, pos_data);
 
-            		gps_pub.publish(gps_msg);
+            update_gps_msg(&gps_msg, pos_data, gps);
 
-            		ros::spinOnce();
+            gps_pub.publish(gps_msg);
 
-            		loop_rate.sleep();
-	                // after desired message is successfully decoded, we can use the information stored in pos_data vector
-	                // right here, or we can do something with it from inside decodeSingleMessage() function(see ublox.h).
-	                // the way, data is stored in pos_data vector is specified in decodeMessage() function of class UBXParser(see ublox.h)
-	                /*printf("GPS Millisecond Time of Week: %.0lf s\n", pos_data[0]/1000);
-	                printf("Longitude: %lf\n", pos_data[1]/10000000);
-	                printf("Latitude: %lf\n", pos_data[2]/10000000);
-	                printf("Height above Ellipsoid: %.3lf m\n", pos_data[3]/1000);
-	                printf("Height above mean sea level: %.3lf m\n", pos_data[4]/1000);
-	                printf("Horizontal Accuracy Estateimate: %.3lf m\n", pos_data[5]/1000);
-	                printf("Vertical Accuracy Estateimate: %.3lf m\n", pos_data[6]/1000);*/
+            ros::spinOnce();
 
-            } else {
-                // printf("Message not captured\n");
-                // use this to see, how often you get the right messages
-                // to increase the frequency you can turn off the undesired messages or tweak ublox settings
-                // to increase internal receiver frequency
-            }
-
-            if (gps.decodeSingleMessage(Ublox::NAV_STATUS, pos_data) == 1)
-            {
-                printf("Current GPS status:\n");
-                printf("gpsFixOk: %d\n", ((int)pos_data[1] & 0x01));
-
-                printf("gps Fix status: ");
-                switch((int)pos_data[0]){
-                    case 0x00:
-                        printf("no fix\n");
-                        break;
-
-                    case 0x01:
-                        printf("dead reckoning only\n");
-                        break;
-
-                    case 0x02:
-                        printf("2D-fix\n");
-                        break;
-
-                    case 0x03:
-                        printf("3D-fix\n");
-                        break;
-
-                    case 0x04:
-                        printf("GPS + dead reckoning combined\n");
-                        break;
-
-                    case 0x05:
-                        printf("Time only fix\n");
-                        break;
-
-                    default:
-                        printf("Reserved value. Current state unknown\n");
-                        break;
-
-                }
-
-                printf("\n");
-
-            } else {
-                // printf("Status Message not captured\n");
-            }
-
+            loop_rate.sleep();
+            // after desired message is successfully decoded, we can use the information stored in pos_data vector
+            // right here, or we can do something with it from inside decodeSingleMessage() function(see ublox.h).
+            // the way, data is stored in pos_data vector is specified in decodeMessage() function of class UBXParser(see ublox.h)
+            /*printf("GPS Millisecond Time of Week: %.0lf s\n", pos_data[0]/1000);
+            printf("Longitude: %lf\n", pos_data[1]/10000000);
+            printf("Latitude: %lf\n", pos_data[2]/10000000);
+            printf("Height above Ellipsoid: %.3lf m\n", pos_data[3]/1000);
+            printf("Height above mean sea level: %.3lf m\n", pos_data[4]/1000);
+            printf("Horizontal Accuracy Estateimate: %.3lf m\n", pos_data[5]/1000);
+	        printf("Vertical Accuracy Estateimate: %.3lf m\n", pos_data[6]/1000);*/
 
             usleep(200);
         }
